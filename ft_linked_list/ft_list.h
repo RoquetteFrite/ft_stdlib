ff7e618600039990181e23a13476b0d7883d0a6b
Lucas <breakerscorp@pm.me>
2026-08-18 23:06:56 +0200

    Add new feature

---
+++ ft_linked_list/ft_list.h
@@ -0,0 +1,7 @@
+#include "ft_t_list.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+t_list  *ft_push_back(t_list *list, void *data);
+t_list  *ft_push(void *data, t_list l);
--- main.c
+++ main.c
@@ -1,7 +1,29 @@
+#include "ft_t_list.h"
+
 #include <stdio.h>
 
-int main(void)
+void	print_elem_int(t_list *elem)
 {
+	printf("%d", *(int*)elem->data);
+}
+
+void	test_list()
+{
+	t_list	*list;
+	int		data;
+
+	list = malloc(sizeof(t_list));
+	if (list == NULL)
+		return (-1);
+	data = 10;
+	list->data = &data;
+	list->next = NULL; 
+	
+}
+
+int	main(void)
+{
+	test_list();
 	printf("test");
 	return 0;
 }
