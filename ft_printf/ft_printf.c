990de24ac69d88d9753c6388cf7c7412eba12f1a
EnzoBoron <enzo1.boron@gmail.com>
2026-08-19 11:32:18 +0000

    Add a things

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,7 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+
 int ft_printf(const char *format, ...)
 {
-    int printed_chars;
+	va_list args;
+	va_start(args, format);
+	int i = 0;
+	int compteur = 0;
+	while (format[i])
+	{
+		if (format[i] == '%')
+			compteur++;
+		i++;
+	}
+	i = 0;
+    	while (i < compteur)
+	{
+		int val = va_arg(args, int);
+		printf("%d\n", val);
+		i++;
+	}
+	return compteur;
+}
 
-    printed_chars = 0;
-    
+int main(void)
+{
+	ft_printf("% % %", 3, 42, 24);
+	return 0;
 }
