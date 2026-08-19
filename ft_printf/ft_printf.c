bc04ccf932ba2e007577f015939052a58ae9cefb
EnzoBoron <enzo1.boron@gmail.com>
2026-08-19 11:47:06 +0000

    add new parameters

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -10,6 +10,11 @@ int ft_printf(const char *format, ...)
 	while (format[i])
 	{
 		if (format[i] == '%')
+		{
+			if (format[i + 1] && formqt[i + 1] == 'd')
+				print_int(args);
+		 if (format[i + 1] && formqt[i + 1] == 's')
+			 print_str(args);
 			compteur++;
 		i++;
 	}
