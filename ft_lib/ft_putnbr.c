0d952191017aee2cdbf0f99458fd5a3d5d9052ea
Lucas <breakerscorp@pm.me>
2026-08-21 22:38:51 +0200

    Add new feature

---
+++ ft_lib/ft_putnbr.c
@@ -0,0 +1,34 @@
+#include "ft_lib.h"
+
+void ft_putnbr_long(long int nbr)
+{
+	char	to_print;
+
+	if (nbr < 0)
+	{
+		write(1, "-", 1);
+		nbr *= -1;
+	}
+	if (nbr < 10)
+	{
+		to_print = nbr += '0';
+		write(1, &to_print, 1);
+	}
+	else
+	{
+		ft_putnbr_long(nbr / 10);
+		to_print = nbr % 10 + '0';
+		write(1, &to_print, 1);
+	}
+}
+
+void	ft_putnbr(int nbr)
+{
+	ft_putnbr_long(nbr);
+}
+
+
+int	main(void)
+{
+	ft_putnbr_long(-2147483648);
+}
