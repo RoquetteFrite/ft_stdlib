19907929cbcc7088e818c08df1358d8c08d0800e
Lucas <breakerscorp@pm.me>
2026-08-22 16:43:38 +0200

    Add new feature

--- ft_lib/ft_putnbr_double.c
+++ ft_lib/ft_putnbr_double.c
@@ -1,41 +1,9 @@
 #include <unistd.h>
+#include <stdio.h> 	// DEBUG DEBUG DEBUG
 
-int	is_int(double nbr)
-{
-	long int	nbr_int;
+#include "ft_lib.h"
 
-	nbr_int = (long int)nbr;
-	return (nbr_int == nbr);
-}
-
-void	ft_putnbr_double(double nbr)
-{
-	char to_print;
-
-	if (is_int(nbr) && nbr < 10)
-	{
-		to_print = nbr + '0';
-		write(1, &to_print, 1);
-	}
-	else if (is_int(nbr))
-	{
-		ft_putnbr_double((long int)nbr / 10);
-		to_print = (long int)nbr % 10 + '0';
-		write(1, &to_print, 1);
-	}
-	else if (nbr < 10 && nbr != 0)
-	{
-		to_print = (long int)nbr + '0';
-		write(1, &to_print, 1);
-		ft_putnbr_double(nbr * 10);
-	}
-	else if (nbr != 0)
-	{
-		ft_putnbr_double(nbr - (long int)nbr);
-	}
-}
-
-void ft_putnbr_dec(double nbr)
+void ft_putnbr_dec(long double nbr)
 {
 	int		nbr_int;
 	char	to_print;
@@ -50,15 +18,62 @@ void ft_putnbr_dec(double nbr)
 	}
 }
 
+int ft_digit_nbr(long int nbr)
+{
+	int	i;
+
+	i = 0;
+	while (nbr != 0)
+	{
+		nbr /= 10;
+		i++;
+	}
+	return (i);
+}
+
+double	ft_round(long double nbr)
+{
+	if (nbr >= ((long int)nbr + 0.5))
+		return ((long int)nbr + 1);
+	else
+		return ((long int)nbr);
+}
+
+int	is_int(double nbr)
+{
+	long int	nbr_int;
+
+	nbr_int = (long int)nbr;
+	return (nbr_int == nbr);
+}
+
+void	ft_putnbr_double(double nbr)
+{
+	long double	long_double_nbr;
+	long int	int_part;
+	long double	dec_part;
+
+	long_double_nbr = nbr;
+	if (long_double_nbr < 0)
+	{
+		long_double_nbr *= -1;
+		write(1, "-", 1);
+	}
+	int_part = (long int)nbr;
+	dec_part = nbr - int_part;
+	ft_putnbr(int_part);
+	write(1, ".", 1);
+	ft_putnbr_dec(dec_part);
+}
+
 #include <stdio.h>
 int main(void)
 {
-	float	test_dec;
+	float		test_dec;
 
 	test_dec = 0.567;
-	ft_putnbr_double(100.4);
+	ft_putnbr_double(test_dec);
 	write(1, "\n", 1);
 	ft_putnbr_dec(test_dec);
-	printf("\n%f\n", test_dec - (int)test_dec);
-	printf("%.20f\n", 0.567);
+	// printf("\ntest_dec: %lf\n", test_dec);
 }
--- ft_lib/ft_putstr.c
+++ ft_lib/ft_putstr.c
@@ -5,7 +5,7 @@ void	ft_putstr(char *str)
 	write(1, str, ft_strlen(str));
 }
 
-int main(void)
-{
-	ft_putstr("YOLOOOOOO");
-}
+// int main(void)
+// {
+// 	ft_putstr("YOLOOOOOO");
+// }
